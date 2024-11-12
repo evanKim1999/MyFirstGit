77b1f376895a42b12f9fa94bb462981f4c98ad56
evan_kim <zone227@naver.com>
2024-11-12 18:29:07 +0900

    Initial Commit

---
+++ MyFirstGit/MyFirstGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstGitApp.swift
+//  MyFirstGit
+//
+//  Created by KIMAC on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
